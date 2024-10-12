f84f46aa12a085811878ad4031d4794fd2e87402
JakubKumer <JakubKumer@interia.pl>
2024-10-12 14:19:16 +0200

    dziala

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -23,3 +23,4 @@ if (PHP_VERSION_ID < 50600) {
 require_once __DIR__ . '/composer/autoload_real.php';
 
 return ComposerAutoloaderInit40aa654f2e66c20881ae0572fe987a10::getLoader();
+return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
